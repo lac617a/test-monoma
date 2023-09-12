59868f129de265f8a827e0c2907846cbd2c7e958
lac617a <botlacrita617@gmail.com>
2023-09-12 09:34:12 -0500

    feat: added new link to imgbb

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['raw.githubusercontent.com']
+    domains: ['i.ibb.co', 'raw.githubusercontent.com']
   },
   webpack(config) {
     config.module.rules.push({
