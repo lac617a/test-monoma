5f2c896a0d014adf63823f7688f38471dbb9e2b6
lac617a <botlacrita617@gmail.com>
2023-09-12 11:07:01 -0500

    fix: remove ActionMap type

--- src/types.ts
+++ src/types.ts
@@ -68,14 +68,3 @@ export type PokemonData = ICommonDataPokemon & {
     }[]
   }[]
 }
-
-export type ActionMap<M extends { [index: string]: any }> = {
-  [Key in keyof M]: M[Key] extends undefined
-    ? {
-        type: Key
-      }
-    : {
-        type: Key
-        payload: M[Key]
-      }
-}
