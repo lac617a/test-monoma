b81d5267db6387f9601d9363006da7f9be16b4e9
lac617a <botlacrita617@gmail.com>
2023-09-12 09:11:11 -0500

    feat: performance Card pokemon

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,13 +1,15 @@
 import './styles.css'
 import React from 'react'
 import Image from 'next/image'
-import { Box, Heading, Stack, Typography, VStack } from 'native-piece'
+import { Box, Button, Heading, Stack, Typography, VStack } from 'native-piece'
 
 // UITLS
 import { PokemonData } from '@/types'
 import typePokemon from '@/constants/type_pokemon'
+import { usePokemon } from '@/context'
 
 export default function Card({ item }: { item: PokemonData }) {
+  const { onSelectPokemon } = usePokemon()
   const types = item.types.slice(0, 2).map(item => item.type.name).join().replace(',', ' / ')
   const moves = item.moves.slice(0, 2).map(item => item.move.name)
 
@@ -19,61 +21,58 @@ export default function Card({ item }: { item: PokemonData }) {
       overflow='hidden'
       backgroundColor='var(--color-white)'
     >
-      <Box
-        height={200}
-        maxHeight={200}
-        minHeight={200}
-        overflow='hidden'
-      >
-        <Stack position='relative' height='100%'>
-          <Box as='figure' width='100%' height='100%'>
-            <Box className='tst-pokemon-back'>
-              <Image
-                width={300}
-                height={300}
-                loading='lazy'
-                alt={item.name}
-                src={item.sprites.back_default}
-              />
-            </Box>
-            <Box position='relative' zIndex={2} height='100%'>
-              <VStack alignItems='center' justifyContent='center' height='100%'>
+      <Button onClick={() => onSelectPokemon(item)} width='100%'>
+        <Box
+          height={200}
+          maxHeight={200}
+          minHeight={200}
+          overflow='hidden'
+        >
+          <Stack position='relative' height='100%'>
+            <Box as='figure' width='100%' height='100%'>
+              <Box className='tst-pokemon-back'>
                 <Image
+                  quality={1}
+                  width={300}
+                  height={300}
                   loading='lazy'
-                  width={150}
-                  height={150}
                   alt={item.name}
-                  src={item.sprites.front_default}
+                  src={item.sprites.back_default || item.sprites.back_shiny}
                 />
-                <Stack
-                  as='figcaption'
-                  zIndex={2}
-                  width='100%'
-                  paddingInline={12}
-                  position='relative'
-                  justifyContent='space-between'
-                >
-                  <Typography fontWeight={700}>{types}</Typography>
-                  <Box backgroundColor='var(--color-primary)' borderRadius={50} padding='6px 12px'>
-                    <Typography fontSize={12} color='var(--color-white)'>{item.weight} / {item.height}</Typography>
-                  </Box>
-                </Stack>
-              </VStack>
+              </Box>
+              <Box position='relative' zIndex={2} height='100%'>
+                <VStack alignItems='center' justifyContent='space-between' height='100%'>
+                  <Image
+                    loading='eager'
+                    unoptimized
+                    width={150}
+                    height={150}
+                    alt={item.name}
+                    src={item.sprites.front_default || item.sprites.front_shiny}
+                  />
+                  <Stack as='figcaption' className='tst-card-figcation'>
+                    <Typography fontWeight={700} color='var(--color-third)'>{types}</Typography>
+                    <Box backgroundColor='var(--color-primary)' borderRadius={50} padding='6px 16px'>
+                      <Typography fontSize={12} color='var(--color-white)'>{item.weight.toFixed(1)} / {item.height.toFixed(1)}</Typography>
+                    </Box>
+                  </Stack>
+                </VStack>
+              </Box>
             </Box>
-          </Box>
-        </Stack>
-      </Box>
-      <Stack padding={12} justifyContent='space-between'>
-        <VStack>
-          <Heading>{item.name}</Heading>
-          <Stack gap={4}>
-            {moves.map((item, index) => (
-              <Typography key={index} color='var(--color-secondary)'>#{item}</Typography>
-            ))}
           </Stack>
-        </VStack>
-        <Box className='tst-type-pokemon' backgroundPosition={typePokemons} />
-      </Stack>
+        </Box>
+        <Stack padding={12} justifyContent='space-between'>
+          <VStack alignItems='flex-start' justifyContent='center'>
+            <Heading>{item.name}</Heading>
+            <Stack gap={4}>
+              {moves.map((item, index) => (
+                <Typography key={index} color='var(--color-secondary)'>#{item}</Typography>
+              ))}
+            </Stack>
+          </VStack>
+          <Box className='tst-type-pokemon' backgroundPosition={typePokemons} />
+        </Stack>
+      </Button>
     </Box>
   )
 }
