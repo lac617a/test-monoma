527b1f1a6bfd3d62d85435318eadda7f5825f40e
lac617a <botlacrita617@gmail.com>
2023-09-12 14:48:29 -0500

    chore: added Cypress

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['i.ibb.co', 'i.pravatar.cc', 'raw.githubusercontent.com']
   },
+  ignorePatterns: [
+    'src/**/*.cy.{ts, tsx}'
+  ],
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
